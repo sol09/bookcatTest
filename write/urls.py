96a1693a4a0f41b1ef973fa8de1a1516c8d12bb4
Yesol Eom <yesoll0927@gmail.com>
2022-01-09 22:15:23 +0900

    0109_리뷰목록조회 완, 리뷰선택조회-1step

--- write/urls.py
+++ write/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .models import userT, reviewT
 
 urlpatterns = [
     path('', views.index),
+    #path(str(reviewT.reviewId)+'/', views.review)
 ]
--- write/views.py
+++ write/views.py
@@ -4,5 +4,11 @@ from .models import reviewT
 def index(request):
     """리뷰 목록 출력"""
     review_list = reviewT.objects.order_by('-reviewId')
-    context = {'rv_list': review_list}
+    context = {'review_list': review_list}
     return render(request, 'write/review_list.html', context)
+
+def review(request):
+    """선택한 리뷰 출력"""
+    rv = reviewT.objects.get(reviewT.reviewId)
+    context = {'rv': rv}
+    return render(request, 'write/review.html', context)
