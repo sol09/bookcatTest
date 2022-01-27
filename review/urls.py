2e04f35bd825cb24f628e6a45e9bc071fa4d926d
Yesol Eom <yesoll0927@gmail.com>
2022-01-27 21:57:54 +0900

    0127/[DRF]get,post 함수생성

--- review/urls.py
+++ review/urls.py
@@ -7,15 +7,18 @@ from django.views.generic import TemplateView
 from django.conf.urls import url
 
 # DB api 보기
+'''
 router = routers.DefaultRouter()
 router.register('review', views.ReviewView, 'Review')
 router.register('user', views.UserView, 'User')
+'''
 
 urlpatterns = [
-    path('', views.list, name='list'),
+    #path('', views.list, name='list'),
     path('write/', views.write, name='write'),
     path('detail/<int:reviewId>/', views.detail, name='detail'),
 
     # DRF 사용하는 url
-    path('api/', include(router.urls))
+    #path('api/', include(router.urls))
+    path('', views.ReviewView.as_view())
 ]
--- review/views.py
+++ review/views.py
@@ -2,18 +2,38 @@ from django.shortcuts import render, redirect
 #from .models import Review, User   # serializers를 통해 DB를 받았기 때문에 models import 불필요
 
 from .serializers import *
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from django.http import JsonResponse
 import json
 
-class ReviewView(viewsets.ModelViewSet):
-    serializer_class = ReviewSerializer
+#class ReviewView(viewsets.ModelViewSet):
+class ReviewView(APIView):
     queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def post(self, request):
+        review_serializer = ReviewSerializer(data=request.data)
+        if review_serializer.is_valid():
+            review_serializer.save()
+            return Response(review_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(review_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, **kwargs):
+        if kwargs.get('reviewId') is None:
+            review_qr_serializer = ReviewSerializer(Review.objects.all(), many=True)
+            return Response(review_qr_serializer.data, status=status.HTTP_200_OK)
+        else:
+            reviewId = kwargs.get('reviewId')
+            review_serializer = ReviewSerializer(Review.objects.get(id=reviewId))
+            return Response(review_serializer.data, status=status.HTTP_200_OK)
 
 class UserView(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
     queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 def index(request):
     return render(request, 'index.html')
